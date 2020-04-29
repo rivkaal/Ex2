b19895dfaa15596a9c6e04ffc883794301ecbaa1
tamarv <tamar.vaitzman@mail.huji.ac.il>
2020-04-29 12:20:01 +0300

    Add some more basic methods in Thread class.

--- Thread.cpp
+++ Thread.cpp
@@ -7,12 +7,29 @@
 Thread::Thread(int idThread)
 {
     this->idThread = idThread;
-
 }
 
 int Thread::getId()
 {
     return idThread;
 }
+
+sigjmp_buf &Thread::getState();
+
+sigjmp_buf &getState();
+
+int Thread::getCountQuantom()
+{
+    return countQuantom;
+}
+
+void Thread::raisinCountQuantom()
+{
+    countQuantom++;
+}
+
+
+
+
 
 
--- Thread.h
+++ Thread.h
@@ -10,17 +10,28 @@ class Thread
 {
 private:
 
+    State state;
     int idThread;
+    int countQuantom;
+    //TODO define stack size. how much size?
 
 public:
 
-    Thread(int idThread);;
+    Thread(int idThread);
 
     //Todo do i need a destructor?
+     // i think we'd need a destructor maybe to delete the stack of the thread when it finishes
+
+    int getId(){};
+
+    sigjmp_buf &getState() const;
+
+    void setState(State state){};
 
 
-    int getId();;
+    int getCountQuantom();;
 
+    void raisinCountQuantom();;
 
 };
 
